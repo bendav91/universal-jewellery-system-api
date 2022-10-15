6e760c946837c2b444ac1695ee046d52296e7eb8
Ben Davies <ben.davies@motorpoint.co.uk>
2022-10-15 14:18:52 +0100

    chore: update order service to return DTO's

--- src/dtos/orders/order.dto.ts
+++ src/dtos/orders/order.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { OrderStatus } from 'src/constants/orders/order-status.enum';
 import { PaymentStatus } from 'src/constants/orders/payment-status.enum';
 import { Order } from 'src/entities/orders/orders.entity';
+import { OrderItemDto } from './order-item.dto';
 
 export class OrderDto implements Readonly<OrderDto> {
   @ApiProperty({
@@ -20,6 +21,7 @@ export class OrderDto implements Readonly<OrderDto> {
   public updatedAt: Date;
   public createdAt: Date;
   public deletedAt: Date;
+  public orderItems: OrderItemDto[];
 
   constructor(partial: Partial<Order>) {
     Object.assign(this, partial);
--- src/features/orders/orders.service.ts
+++ src/features/orders/orders.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateOrderDto } from 'src/dtos/orders/create-order.dto';
+import { OrderDto } from 'src/dtos/orders/order.dto';
 import { PageMetaDto } from 'src/dtos/page/page-meta.dto';
 import { PageOptionsDto } from 'src/dtos/page/page-options.dto';
 import { PageDto } from 'src/dtos/page/page.dto';
@@ -15,26 +16,46 @@ export class OrdersService {
     private readonly ordersRepository: Repository<Order>,
   ) {}
 
-  async getOrders(pageOptionsDto: PageOptionsDto): Promise<PageDto<Order>> {
+  async getOrders(pageOptionsDto: PageOptionsDto): Promise<PageDto<OrderDto>> {
     const queryBuilder = this.ordersRepository.createQueryBuilder('order');
     queryBuilder
       .orderBy('order.createdAt', pageOptionsDto.sortOrder)
+      .leftJoinAndSelect('order.orderItems', 'orderItem')
       .skip(pageOptionsDto.skip)
       .take(pageOptionsDto.take);
 
     const itemCount = await queryBuilder.getCount();
     const { entities } = await queryBuilder.getRawAndEntities();
+
+    const orderDtos = entities.map((entity) => {
+      return new OrderDto({
+        createdAt: entity.createdAt,
+        updatedAt: entity.updatedAt,
+        deletedAt: entity.deletedAt,
+        orderNumber: entity.orderNumber,
+        orderItems: entity.orderItems,
+        status: entity.status,
+      });
+    });
+
     const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
-    return new PageDto(entities, pageMetaDto);
+    return new PageDto(orderDtos, pageMetaDto);
   }
 
-  async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
-    return new Order(
-      await this.ordersRepository.save({
-        orderNumber: generateOrderNumber(),
-        ...createOrderDto,
-      }),
-    );
+  async createOrder(createOrderDto: CreateOrderDto): Promise<OrderDto> {
+    const newOrder = await this.ordersRepository.save({
+      orderNumber: generateOrderNumber(),
+      ...createOrderDto,
+    });
+
+    return new OrderDto({
+      createdAt: newOrder.createdAt,
+      updatedAt: newOrder.updatedAt,
+      deletedAt: newOrder.deletedAt,
+      orderNumber: newOrder.orderNumber,
+      orderItems: newOrder.orderItems,
+      status: newOrder.status,
+    });
   }
 
   async getOrderByOrderNumber(orderNumber: string): Promise<Order> {
